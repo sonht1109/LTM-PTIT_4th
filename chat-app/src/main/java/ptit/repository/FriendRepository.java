cca9300fd3e815ad8ae53324c3e8f3cc567f5cb9
phamthainb <phamthai4643@gmail.com>
2021-09-21 00:54:25 +0700

    [code] fix list friend 1
    Signed-off-by: phamthainb <phamthai4643@gmail.com>

--- chat-app/src/main/java/ptit/repository/FriendRepository.java
+++ chat-app/src/main/java/ptit/repository/FriendRepository.java
@@ -10,7 +10,7 @@ public interface FriendRepository extends JpaRepository<FriendEntity, Long> {
    @Query(value = "select friend from FriendEntity friend where (friend.user_id_1.id=?1 and friend.user_id_2.id=?2) or (friend.user_id_2.id=?1 and friend.user_id_1.id=?2)")
    FriendEntity findFirstByUser_id_1AndUser_id_2(Long user_id_1, Long user_id_2);
 
-   @Query(value = "select friend from FriendEntity friend where friend.user_id_2.id=:userId and friend.user_id_1.username like %:username% and friend.confirmed=true")
+   @Query(value = "select friend from FriendEntity friend where (friend.user_id_2.id=:userId and friend.user_id_1.username like %:username%) or (friend.user_id_1.id=:userId and friend.user_id_2.username like %:username%) and friend.confirmed=true")
    List<FriendEntity> findAllByUsernameFriend(Long userId, String username);
 
    @Query(value = "select friend from FriendEntity friend where friend.user_id_2.id=:userId and friend.user_id_1.username like %:username% and friend.confirmed=false")
